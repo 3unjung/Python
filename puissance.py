8bdbd852accc77fb44061dd08d3c3efa28622b45
3unjung <59709579+3unjung@users.noreply.github.com>
2020-03-11 09:55:54 +0100

    add function exponentiel

---
+++ puissance.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 10 21:51:45 2020
+
+@author: Eunjung
+"""
+
+'''def myexp(base, exponent):
+    result = 1
+    for i in range(exponent):
+        result = result * base
+    return result
+
+'''
+
+
+def puissance(base, exposant):
+    result = 1
+    for i in range(exposant):
+        result = result * base
+    return print(result)
+
+
+puissance(10, 5)
+
